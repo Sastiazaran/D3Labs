5d7e3e83ab87708b3f9172f4ff691a4255c5fff4
Sastiazaran <sastiazaran2000@gmail.com>
2023-05-31 17:25:15 -0600

    d3

--- 11_StackedAreaChart/js/main.js
+++ 11_StackedAreaChart/js/main.js
@@ -14,41 +14,36 @@ var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + 
     	"," + margin.top + ")");
 
-// Time parser for x-scale
+
 var parseDate = d3.timeParse('%Y');
 var formatSi = d3.format(".3s");
 var formatNumber = d3.format(".1f"),
 formatBillion = (x) => { return formatNumber(x / 1e9); };
 
-// Scales
 var x = d3.scaleTime().rangeRound([0, width]);
 var y = d3.scaleLinear().rangeRound([height, 0]);
 var color = d3.scaleOrdinal(d3.schemeSpectral[11]);
 
-// Axis generators
+
 var xAxisCall = d3.axisBottom();
 var yAxisCall = d3.axisLeft().tickFormat(formatBillion);
 
-// Area generator
-// TODO: create the area generator.
 var area = d3.area()
     .x( (d) => x(d.data.date))
     .y0( (d) => y(d[0]))
     .y1( (d) => y(d[1]));
 
-// The x coordinate will be the date of the data
-// while y0 and y1 will be in the 0 and 1 positions of the d element
-// TODO: create the stack
+
 var stack = d3.stack();
 
-// Axis groups
+
 var xAxis = g.append("g")
 	.attr("class", "x axis")
     .attr("transform", "translate(0," + height + ")");
 var yAxis =  g.append("g")
 	.attr("class", "y axis");
         
-// Y-Axis label
+
 yAxis.append("text")
 	.attr("class", "axis-title")
     .attr("fill", "#000")
@@ -58,7 +53,7 @@ yAxis.append("text")
     .attr("text-anchor", "end")
     .text("Billions of liters");
 
-// Legend code
+
 var legend = g.append("g")
     .attr("transform", "translate(" + (width + 150) + 
         "," + (height - 210) + ")");
@@ -69,7 +64,7 @@ d3.csv('data/stacked_area2.csv').then((data) => {
         return key !== 'date'; 
     }));
         
-    //TODO: obtain the keys array, remember to remove the first column
+    
     var keys = d3.keys(data[0]).filter((key) => {
         return key != 'date';
     })
@@ -88,19 +83,15 @@ d3.csv('data/stacked_area2.csv').then((data) => {
     x.domain(d3.extent(data, (d) => d.date));
     y.domain([0, maxDateVal]);
 
-    // Generate axes once scales have been set
+ 
     xAxis.call(xAxisCall.scale(x))
     yAxis.call(yAxisCall.scale(y))
 
-    // Add stacked area chart
-    // TODO: finish the configuration of the stack object
-    // by setting the keys, order and offset
+ 
     stack.keys(keys)
         .order(d3.stackOrdenNone)
         .offset(d3.stackOffsetNone);
 
-    // TODO: bind the data to the stack and create the group 
-    // that will contain the area path
     var browser = g.selectAll('.browser')
         .data(stack(data))
         .enter()
@@ -108,14 +99,11 @@ d3.csv('data/stacked_area2.csv').then((data) => {
                 .attr('class', (d) => { return 'browser ' + d.key;})
                 .attr('fill-opacity', 0.5);
 
-    // TODO: call the area generator with the appropriate data
     browser.append('path')
         .attr('class', 'area')
         .attr('d', area)
         .style('fill', (d) => { return color(d.key); });
 
-    // Create legend
-    // TODO: Create a legend showing all the names of every color
     let legends = g.append('g')
         .attr("transform", "translate(" + (width - 20) + "," + (height - 225) +")");
 
